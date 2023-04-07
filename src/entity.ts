e0e9114e6daf272397707a0a57600f3a5fd2df9f
Richard Campen <richard.campen@octave.nz>
2023-04-07 21:37:06 +1200

    feat: start separating lib code from use of lib code.

--- src/entity.ts
+++ src/entity.ts
@@ -1,14 +1,7 @@
-import { multiply } from '../wasm/index.wasm';
-
-
 export default class Entity {
     constructor(scene) {
         this.scene = scene;
         this._mesh;
-        // todo: make the following a _transform.
-        this.translation = [0, 0];
-        this.moveSpeed = 100;
-        this.time = 0;
     }
 
     setMesh(mesh) {
@@ -16,12 +9,11 @@ export default class Entity {
     }
 
     update() {
-        console.log('updating entity: ', this.translation );
-        var deltaTime = this.scene.engine.time - this.time;
-        this.time = this.scene.engine.time;
-        if (this.translation[0] < multiply(this.scene.engine.canvas.height * 1.0, 0.9))
-        this.translation = [this.translation[0] + deltaTime * this.moveSpeed, this.translation[1] + deltaTime * this.moveSpeed];
-        // todo: separate parent class from the logic above.
+        // do things here.
+        this.postUpdate();
+    }
+
+    postUpdate() {
         if (this._mesh) {
             this._mesh.update()
         }
